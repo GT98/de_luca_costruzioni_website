297066fc9ffdd20906c7079b53b4204654440dd9
gennytorelli <gennytorelli98@gmail.com>
2025-11-18 23:34:56 +0100

    creazione sezione chi siamo

---
+++ src/components/who-we-are/who-we-are.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Button } from "../button/button";
+
+@Component({
+  selector: 'app-who-we-are',
+  imports: [Button],
+  templateUrl: './who-we-are.html',
+  styleUrl: './who-we-are.scss',
+})
+export class WhoWeAre {
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Button } from "../../components/button/button";
 import { FollowOn } from "../../components/follow-on/follow-on";
 import { BannerCta } from "../../components/banner-cta/banner-cta";
+import { WhoWeAre } from "../../components/who-we-are/who-we-are";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss',
-  imports: [Button, FollowOn, BannerCta]
+  imports: [Button, FollowOn, BannerCta, WhoWeAre]
 })
 export default class Home { }
